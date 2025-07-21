227442f0caf42b7168cbac08b28c81151910b41e
Elie <79168733+el2zay@users.noreply.github.com>
2025-07-21 15:13:15 +0200

    more logical name + new function

---
+++ lib/utils/screen_utils.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+import 'package:chalkdart/chalk.dart';
+import 'package:letters_game/letters_game.dart';
+
+void enableAlternateScreen() {
+  stdout.write('\x1b[?1049h');
+  setCursorPosition((terminalHeight * 0.18).floor(), 0);
+}
+
+void disableAlternateScreen() {
+  stdout.write('\x1b[?1049l');
+
+  stdout.write('\x1b[?25h');
+}
+
+void setCursorPosition(int row, int col) {
+  stdout.write('\x1b[0;0H');
+  stdout.write('\x1b[$row;${col}H');
+}
+
+void clearScreen() {
+  stdout.write('\x1b[2J');
+  setCursorPosition(0, 0);
+}
+
+void colorScreen(Chalk color) {
+  for (int i = 0; i < terminalHeight; i++) {
+    setCursorPosition(i + 1, 0);
+    stdout.write(color(' ' * terminalWidth));
+  }
+}
