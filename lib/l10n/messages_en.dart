70c651334618c5145d53cdf051ca93b743cec0cf
Elie <79168733+el2zay@users.noreply.github.com>
2025-08-05 22:53:51 +0200

    New translations

--- lib/l10n/messages_en.dart
+++ lib/l10n/messages_en.dart
@@ -34,5 +34,23 @@ class MessageLookup extends MessageLookupByLibrary {
         "not_buildable": MessageLookupByLibrary.simpleMessage("The word cannot be formed with the available letters."),
         "terminal_size_error":
             MessageLookupByLibrary.simpleMessage("The size of your terminal is too small. Min height: 33 | width: 156"),
+        "how_to_play_description": MessageLookupByLibrary.simpleMessage(
+            "The game gives you letters. Use these letters to form one of the longest words possible.\nYou can use the same letter several times, but it's best to use it as many times as the game suggests. \nBy default, the game will always suggest 9 letters with a minimum of 2 vowels.\n\nThe game uses dictionaries of words found on the Internet. As a result, there may be abbreviations, old words or errors. \nIn French, the dictionary does not take into account words with accents.\nDictionaries can be found at https://files.bassinecorp.fr/letters_game/\n\nThe game uses no API and requires no Internet connection.\n\n"),
+        "press_any_key": MessageLookupByLibrary.simpleMessage("Press any key to continue."),
+        "developed_by": MessageLookupByLibrary.simpleMessage("Developed with <3 in Paris by Elie."),
+        "how_to_play": MessageLookupByLibrary.simpleMessage("1. How to play? "),
+        "main_menu": MessageLookupByLibrary.simpleMessage("2. Main menu    "),
+        "settings": MessageLookupByLibrary.simpleMessage("3. Settings     "),
+        "min_letters_count": MessageLookupByLibrary.simpleMessage("Minimum letters count: "),
+        "currently": (count) => "Currently : $count",
+        "min_vowels_count": MessageLookupByLibrary.simpleMessage("Minimum vowels count: "),
+        "invalid_number": (input) => "The input '$input' is not a valid number.",
+        "quit_settings": MessageLookupByLibrary.simpleMessage("Press Q to quit."),
+        "quit": MessageLookupByLibrary.simpleMessage("4. Quit          "),
+        "enter_value": MessageLookupByLibrary.simpleMessage("Enter a value: "),
+        "invalid_min_letters_count":
+            MessageLookupByLibrary.simpleMessage("The number of letters must be between 6 and 20."),
+        "invalid_min_vowels_count": MessageLookupByLibrary.simpleMessage(
+            "The number of vowels must be between 2 and the number of letters minus 2.")
       };
 }
